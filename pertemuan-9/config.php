51cee8b816978831584a1cfd2357d119fe01e614
raimongg <raiyhanxd@gmail.com>
2025-11-22 17:14:32 +0700

    Add configuration file for database connection

---
+++ pertemuan-9/config.php
@@ -0,0 +1,18 @@
+config.php
+
+
+<?php
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "pendaftaran_siswa";
+$port = 4306;
+
+$db = mysqli_connect($server, $user, $password, $nama_database, $port);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
+?>
